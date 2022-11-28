243e24e348d7e7c048b7ae9ec4eb6557889daf77
Kenn0bi <schegol@icloud.com>
2022-11-29 00:43:37 +0500

    add defs for POST, PUT and DELETE methods, and some code optimization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from random import choice
+from flask import request
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -34,35 +35,67 @@ quotes = [
 
 ]
 
+def find_quote(quote_id):
+    for quote in quotes:
+        if (quote["id"] == quote_id):
+            return quote
+
+
 @app.route("/about")
 def about():
    return about_me
 
 @app.route("/")
 def hello_world():
-   return "Hello, World!"
+    return "Hello, World!"
 
 @app.route("/quotes")
 def get_quotes():
-   return quotes
+    return quotes
 
-@app.route("/quote/<int:quote_id>")
+@app.route("/quotes/<int:quote_id>")
 def get_quote_by_id(quote_id):
-   for quote in quotes:
-      if ( quote["id"] == quote_id ):
-         return quote
-   return f"Quote with id={quote_id} not found", 404
+    quote = find_quote(quote_id)
+    if quote:
+        return quote
+    return f"Quote with id={quote_id} not found", 404
 
 @app.route("/quotes/count")
 def get_quotes_count():
    return { "quotes": len(quotes) }
 
 
-@app.route("/quote/random")
+@app.route("/quotes/random")
 def get_random_quote():
    return choice(quotes)
 
+@app.route("/quotes", methods=['POST'])
+def create_quote():
+   data = request.json
+   new_quote = data
+   new_quote['id'] = quotes[-1]['id'] + 1
+   quotes.append(new_quote)
+   print("data = ", data)
+   return new_quote, 201
+
+@app.route("/quotes/<int:quote_id>", methods=['PUT'])
+def edit_quote(quote_id):
+    new_data = request.json
+    quote = find_quote(quote_id)
+    if quote:
+        for key in new_data:
+            quote[key] = new_data[key]
+        return '', 200
+    return f"Quote with id={quote_id} not found", 404
+
+@app.route("/quotes/<int:quote_id>", methods=['DELETE'])
+def del_quote(quote_id):
+    quote = find_quote(quote_id)
+    if quote:
+        quotes.remove(quote)
+        return f"Quote with id {quote_id} is deleted.", 200
+    return f"Quote with id={quote_id} not found", 404
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
 
